509f0bc51e5d5e0dbacaf0449988cdfc1747b381
taehwa015 <taehwa015@gmail.com>
2023-08-16 13:40:56 -0400

    init

--- vignettes/knit_vignette.R
+++ vignettes/knit_vignette.R
@@ -1,9 +1,7 @@
 library(knitr)
-knit("vignettes/glca.Rmd.orig", output = "vignettes/glca.Rmd")
-
 # remove file path vignettes
-replace <- readLines("vignettes/glca.Rmd")
-replace <- gsub("<img src=\"vignettes/", "<img src=\"", replace)
-fileConn <- file("vignettes/glca.Rmd")
+replace = readLines("vignettes/drRML.Rmd")
+replace = gsub("<img src=\"vignettes/", "<img src=\"", replace)
+fileConn = file("vignettes/drRML.Rmd")
 writeLines(replace, fileConn)
 close(fileConn)
